02bac1eb24a821660c00080640d3ee821cfeaf03
Nabeel <nabeelfahad41@gmail.com>
2026-08-31 17:13:18 +0530

    update recent changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,16 @@ const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60 * 60 * 24 * 30,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   },
 };
 
